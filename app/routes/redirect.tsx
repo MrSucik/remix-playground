0b14d6cc77cc9f274a59aa699f953f7712e8a64c
Daniel Suchan <mr.sucik@gmail.com>
2022-11-30 12:46:40 +0900

    feat: redirect endpoint

---
+++ app/routes/redirect.tsx
@@ -0,0 +1,26 @@
+const isIOS = () =>
+  [
+    "iPad Simulator",
+    "iPhone Simulator",
+    "iPod Simulator",
+    "iPad",
+    "iPhone",
+    "iPod",
+  ].includes(navigator.platform) ||
+  // iPad on iOS 13 detection
+  navigator.userAgent.includes("Mac");
+export const googlePlayUrl =
+  "https://play.google.com/store/apps/details?id=xyz.enter.app";
+export const appStoreUrl =
+  "https://apps.apple.com/app/enter-nft-token-gating/id1623372424";
+
+export default function Index() {
+  if (typeof window !== "undefined") {
+    const isAppleDevice = isIOS();
+    console.log(isAppleDevice);
+
+    window.location.replace(isAppleDevice ? appStoreUrl : googlePlayUrl);
+  }
+
+  return <></>;
+}
