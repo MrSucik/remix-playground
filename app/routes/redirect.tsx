3a335bbb7b9cc653bcd4da0b4d6d4bc96c90b446
Daniel Suchan <mr.sucik@gmail.com>
2022-11-30 13:31:58 +0900

    hmm

--- app/routes/redirect.tsx
+++ app/routes/redirect.tsx
@@ -1,39 +1,26 @@
-const isIOS = () =>
-  [
-    "iPad Simulator",
-    "iPhone Simulator",
-    "iPod Simulator",
-    "iPad",
-    "iPhone",
-    "iPod",
-  ].includes(navigator.platform) ||
-  // iPad on iOS 13 detection
-  navigator.userAgent.includes("Mac");
-export const googlePlayUrl =
-  "https://play.google.com/store/apps/details?id=xyz.enter.app";
+const appLink =
+  "https://apps.apple.com/app/enter-nft-token-gating/id1623372424";
 
 const red = () => {
-  window.open("https://apps.apple.com/app/enter-nft-token-gating/id1623372424");
+  location.replace(appLink);
+  setTimeout(() => {
+    window.open(appLink);
+  }, 300);
 };
 
 export default function Index() {
   if (typeof window !== "undefined") {
     setTimeout(() => {
       red();
-    }, 1000);
+    }, 300);
   }
 
   return (
     <>
       redirect to{" "}
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://apps.apple.com/app/enter-nft-token-gating/id1623372424"
-      >
-        redirect
+      <a href="https://apps.apple.com/app/enter-nft-token-gating/id1623372424">
+        https://apps.apple.com/app/enter-nft-token-gating/id1623372424
       </a>
-      <div onClick={() => red()}>asd</div>
     </>
   );
 }
