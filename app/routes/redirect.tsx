2f430eaf4e337f6db02475d68e9f76ec145fe0fd
Daniel Suchan <mr.sucik@gmail.com>
2022-11-30 13:23:53 +0900

    hack

--- app/routes/redirect.tsx
+++ app/routes/redirect.tsx
@@ -13,10 +13,33 @@ export const googlePlayUrl =
   "https://play.google.com/store/apps/details?id=xyz.enter.app";
 
 const red = () => {
-  document.location.replace(
+  windowLocation(
     "https://apps.apple.com/app/enter-nft-token-gating/id1623372424"
   );
 };
+
+function windowLocation(url: string) {
+  var X = setTimeout(function () {
+    window.location.replace(url);
+    return true;
+  }, 300);
+  //   @ts-ignore
+  if ((window.location = url)) {
+    clearTimeout(X);
+    return true;
+  } else {
+    //   @ts-ignore
+    if ((window.location.href = url)) {
+      clearTimeout(X);
+      return true;
+    } else {
+      clearTimeout(X);
+      //   @ts-ignore
+      window.location.replace(url);
+      return true;
+    }
+  }
+}
 
 export default function Index() {
   if (typeof window !== "undefined") {
